4b3587ba6a29c0649100674e0c0eaaccd4afec95
Arlid Henao Rueda <arliid@hotmail.com>
2019-08-01 18:49:20 -0500

    Initial Commit

---
+++ Jobs App/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Jobs App
+//
+//  Created by Arlid Henao Rueda on 8/1/19.
+//  Copyright © 2019 Arlid Henao Rueda. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
