f60f66814887264b1957aff309153b056d3d1689
Arlid Henao Rueda <arliid@hotmail.com>
2019-08-06 17:37:56 -0500

    [update] button View Recovery

--- Jobs App/Application/Authentication/Recovery/RecoveryViewController.swift
+++ Jobs App/Application/Authentication/Recovery/RecoveryViewController.swift
@@ -12,8 +12,21 @@ class RecoveryViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        //crear boton de volver atras (1)
+        let leftBtn = UIButton(type: .custom)
+        leftBtn.setImage(UIImage(named: Constants.Texts.SignUp.iconBehind), for: .normal)
+        leftBtn.imageView?.contentMode = .scaleAspectFit
+        leftBtn.addTarget(self, action: #selector(goToLogin(sender:)), for: .touchUpInside)
+        let leftBarButton = UIBarButtonItem(customView: leftBtn)
+        self.navigationItem.leftBarButtonItem = leftBarButton
 
         // Do any additional setup after loading the view.
+    }
+    
+    // se crea la funcion que llama el storyboard cuando se presiona el boton de atras (1)
+    @objc func goToLogin(sender: UIButton) {
+        dismiss(animated: true, completion: nil)
     }
     
 
