67f5f6bf22f143e65971c117422487df0e82eb06
Arlid Henao Rueda <arliid@hotmail.com>
2019-08-07 08:41:17 -0500

    [add] File RecoveryViewModel

---
+++ Jobs App/Application/Authentication/Recovery/RecoveryViewModel.swift
@@ -0,0 +1,13 @@
+//
+//  RecoveryViewModel.swift
+//  Jobs App
+//
+//  Created by Arlid Henao Rueda on 8/7/19.
+//  Copyright © 2019 Arlid Henao Rueda. All rights reserved.
+//
+
+import Foundation
+
+class RecoveryViewModel {
+
+}
