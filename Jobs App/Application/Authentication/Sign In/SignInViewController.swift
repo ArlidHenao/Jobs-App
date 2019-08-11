155bc20b44abadd1abb59c9a48eee620102abca9
Arlid Henao Rueda <arliid@hotmail.com>
2019-08-10 21:48:52 -0500

    [update] project structure

--- Jobs App/Application/Authentication/Sign In/SignInViewController.swift
+++ Jobs App/Application/Authentication/Sign In/SignInViewController.swift
@@ -159,12 +159,4 @@ class SignInViewController: UIViewController {
     }
     //fin
     
-    //funcion para validar con una expresion regular si el email o password es valido
-    private func validate(text: String, regex: String) -> Bool{
-        let range = NSRange(location: 0, length: text.count)
-        let regex = try? NSRegularExpression(pattern: regex)
-        return regex?.firstMatch(in: text, options: [], range: range) != nil
-    }
-    //fin
-    
 }
